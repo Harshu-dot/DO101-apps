e536dcfe5d3f1e14342b4c8bb674b9694a5aded8
Namrata Shinde <142914293+namratapshinde48@users.noreply.github.com>
2025-01-20 04:35:27 +0530

    Update app.js
    Added comments to explain the Hello World script

--- hello-nodejs/app.js
+++ hello-nodejs/app.js
@@ -1,2 +1,5 @@
-console.log("Hello World!\n");
+// This script prints "Hello World!" to the console
+
+// The console.log() function outputs the specified string to the console
+console.log("Hello World!\n"); // The \n adds a new line after the message
 
